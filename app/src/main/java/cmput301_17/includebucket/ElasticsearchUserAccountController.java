0ccb39767050f0130d31f0ea8c19a56d9550d335
michelleTagarino <mctagarino@gmail.com>
2016-11-02 13:39:19 -0600

    Added Jest library (.jar file) in app/src/include and ElasticsearchUserAccountController class

---
+++ app/src/main/java/cmput301_17/includebucket/ElasticsearchUserAccountController.java
@@ -0,0 +1,27 @@
+package cmput301_17.includebucket;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+
+/**
+ * Created by michelletagarino on 16-11-02.
+ */
+public class ElasticsearchUserAccountController {
+
+    private static JestDroidClient client;
+
+
+}
