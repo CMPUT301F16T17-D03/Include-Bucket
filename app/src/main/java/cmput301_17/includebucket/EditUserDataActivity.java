594f90a67308bea42d2202aa74f7e12ff6fc3908
LaneScobie <scobie@ualberta.ca>
2016-11-19 11:58:42 -0700

    got rid of names- not in requirements

--- app/src/main/java/cmput301_17/includebucket/EditUserDataActivity.java
+++ app/src/main/java/cmput301_17/includebucket/EditUserDataActivity.java
@@ -23,7 +23,6 @@ public class EditUserDataActivity extends MainMenuActivity {
 
         user = (UserAccount) getIntent().getSerializableExtra("User");
 
-        userName  = (EditText) findViewById(R.id.editName);
         userEmail = (EditText) findViewById(R.id.editEmail);
         userPhone = (EditText) findViewById(R.id.editPhone);
 
@@ -40,7 +39,6 @@ public class EditUserDataActivity extends MainMenuActivity {
     public void editUser() {
 
         String login = user.getUniqueUserName();
-        String name  = userName.getText().toString();
 
         String email;
         String phone;
@@ -56,7 +54,7 @@ public class EditUserDataActivity extends MainMenuActivity {
             phone = userPhone.getText().toString();
         }
 
-        UserAccount user = new UserAccount(login, name, email, phone);
+        UserAccount user = new UserAccount(login, email, phone);
 
         ElasticsearchUserController.CreateUser editUser;
         editUser = new ElasticsearchUserController.CreateUser();
